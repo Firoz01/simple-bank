87a7912777d757ae73037c27a2b55a750efafc2e
Md Firoz Mia <67627130+Firoz01@users.noreply.github.com>
2025-03-08 15:52:54 +0600

    fix: check error with errors package

--- api/account.go
+++ api/account.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"database/sql"
+	"errors"
 	"net/http"
 
 	db "github.com/Firoz01/simple-bank/db/sqlc"
@@ -48,7 +49,7 @@ func (server *Server) getAccount(ctx *gin.Context) {
 
 	account, err := server.store.GetAccount(ctx, req.ID)
 	if err != nil {
-		if err == sql.ErrNoRows {
+		if errors.Is(err, sql.ErrNoRows) {
 			ctx.JSON(http.StatusNotFound, errorResponse(err))
 		}
 
